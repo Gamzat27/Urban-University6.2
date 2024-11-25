34b699e09ebf6b57c8f70bd1942881cb41d6ac6d
Gamzat27 <gamzat27@icloud.com>
2024-11-25 17:06:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+immutable_var_tuple = ([1,2,3,4],'sveta')
+print(immutable_var_tuple)
+print(immutable_var_tuple [0])
+#immutable_var_tuple [0] = [200]    Если я попробую изменить этот элемент, то компьютер выдаст
+#print(immutable_var_tuple)    ошибку. Потому что,Кортеж не поддерживает обращение по элементам.
+
+mutable_list = ['string','armani', 1,2,3,4,5]
+print(mutable_list)
+mutable_list [0]=[9]
+print(mutable_list)
+
+
